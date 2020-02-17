4ef506ab3981d2e0bbcb855c98e84c657953f9a6
sandeep2suri <60914874+sandeep2suri@users.noreply.github.com>
2020-02-17 17:05:53 +0530

    Create install.sh
    updated file to add third part framework changes

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo /home/ec2-user/istio-install/istio-1.4.3/bin/istioctl manifest apply --set profile=demo
+
+sudo /usr/bin/kubectl apply -f ./metrics-server-0.3.6/deploy/1.8+/
+
+cd ./src
+sudo /usr/bin/kubectl apply -f grafana.yaml
+sudo /usr/bin/kubectl apply -f kaili.yaml
+sudo /usr/bin/kubectl apply -f prometheus.yaml
+sudo /usr/bin/kubectl apply -f tracing.yaml
