8d59b27f09ac298051522ee12bacac46b047a8f7
sandeep2suri <60914874+sandeep2suri@users.noreply.github.com>
2020-02-17 17:16:02 +0530

    Update install.sh
    made some code optimizations

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 set -xv
 
-home_dir=/var/lib/jenkins/workspace/Third-party-framework
+home_dir=/var/lib/jenkins/workspace/Third-party-framework/src
 cd $home_dir
 
-sudo /home/ec2-user/istio-install/istio-1.4.3/bin/istioctl manifest apply --set profile=demo
-
+# install kubernetes metrics server
 sudo /usr/bin/kubectl apply -f ./metrics-server-0.3.6/deploy/1.8+/
 
-cd ./src
+# install istio with demo profile, it will install prometheus, grafana, kaili and jaeger for us
+sudo /home/ec2-user/istio-install/istio-1.4.3/bin/istioctl manifest apply --set profile=demo
+
+# create virtual services for grafana, kaili, prometheus and jaegar
 sudo /usr/bin/kubectl apply -f grafana.yaml
 sudo /usr/bin/kubectl apply -f kaili.yaml
 sudo /usr/bin/kubectl apply -f prometheus.yaml
