cee257c0ddf1fecb2e3b1b81a0d9f13b3137ed24
sandeep2suri <60914874+sandeep2suri@users.noreply.github.com>
2020-02-17 17:12:48 +0530

    Update install.sh
    updated folder name to fix path issue

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+set -xv
+
+home_dir=/var/lib/jenkins/workspace/Third-party-framework
+cd $home_dir
+
 sudo /home/ec2-user/istio-install/istio-1.4.3/bin/istioctl manifest apply --set profile=demo
 
 sudo /usr/bin/kubectl apply -f ./metrics-server-0.3.6/deploy/1.8+/
